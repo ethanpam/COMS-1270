a875acb72d4c78643228e0b535dd9dbd44d6001f
Ethan Pham <ethanpham@Ethans-MacBook-Pro.local>
2025-09-02 18:59:55 -0500

    Initial phase of zanyTexts.py

---
+++ zanyTexts.py
@@ -0,0 +1,12 @@
+#Ethan Tan Phuc Pham          9-2-2025            COMS-1270 Section 4
+#Assignment 1
+#Zany Texts! An interactive text-based adventure game
+
+print("Zany Text!\n")
+print("By: Ethan Pham")
+print("[COMS 1270 Section 4]\n")
+
+noun = input("Enter an noun: ")
+print(noun)
+
+print(f"Once upon of time there was a {noun}"))
