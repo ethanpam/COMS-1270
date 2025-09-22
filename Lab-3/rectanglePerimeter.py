379aaf0eeac5ebac7932ddea5a46f42bbf85cfd6
ethanpam <ethanpham652@gmail.com>
2025-09-21 23:08:53 -0500

    Unfinished Lab 3 sections

---
+++ Lab-3/rectanglePerimeter.py
@@ -0,0 +1,20 @@
+#Ethan Pham        9-11-25
+#Lab 2 - This code calculates perimeter by representing the total distance around with length + width multipied by 2
+
+import math
+
+def rp(l, w):
+    # Citation - https://www.smartick.com/blog/mathematics/geometry/how-to-calculate-perimeters-1/
+    # Citation - Author: Smartick
+    # Citation - Date Written - 6-3-2019
+    # Citation - Date Accessed: 9-11-25
+    return 2 * (l + w)
+
+def main():
+    length = int(input("Enter the length of the rectangle: "))
+    width = int(input("Enter the width of the rectangle: "))
+    answer = rp(length, width)
+    print(f"Your area was {answer}")
+
+if __name__ == "__main__":
+    main()
