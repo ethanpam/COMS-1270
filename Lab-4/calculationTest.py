3803fb249bbe49ed0dccbbd1b55fac733b7e7b9c
ethanpam <ethanpham652@gmail.com>
2025-10-02 22:11:45 -0500

    Finished lab 4

--- Lab-4/calculationTest.py
+++ Lab-4/calculationTest.py
@@ -1,22 +1,104 @@
 # Ethan Pham        9-25-25        Lab B
-
+# This calculation uses imports from different calculations to create one massive calculation from different subjects such as 'myShapes', 'myPhysics', 'myOhms', and 'myFinances'
 import myShapes
 import myPhysics
 import myOhmsLaw
 import myFinances
 
 def main():
-    while True:
-        path = int(input("Which module would you like to enter? \n[1]myShapes \n[2]myPhysics \n[3]myOhmsLaw \n[4]myFinances\n"))
-        if path == 1:
-            section = int(input("Which section would you like to calculate? \n[1]areaOfRectangle \n[2]rectanglePerimeter \n[3]areaOfCircle \n[4]circleCircumference \n"))
-            if section == 1:
-                base = int(input("Enter the length of the base: "))
-                height = int(input("Enter the height of the rectangle: "))
-                answer = myShapes.areaOfRectangle(base, height)
-                print(f"Your area was {answer}")
-                break
-            break
+    setting = True
+    while setting:
+        path = input("Which module would you like to enter? \n[1]myShapes \n[2]myPhysics \n[3]myOhmsLaw \n[4]myFinances \n[q]uit\n")
+        if path == "1":
+            while setting:
+                section = input("Which section would you like to calculate? \n[1]areaOfRectangle \n[2]rectanglePerimeter \n[3]areaOfCircle \n[4]circleCircumference \n[q]uit\n")
+                if section == "1":
+                    base = int(input("Enter the length of the base: "))
+                    height = int(input("Enter the height of the rectangle: "))
+                    answer = myShapes.areaOfRectangle(base, height)
+                    setting = printAnswer(answer)
+                if section == "2":
+                    length = int(input("Enter the length of the rectangle: "))
+                    width = int(input("Enter the width of the rectangle: "))
+                    answer = myShapes.rectanglePerimeter(length, width)
+                    setting = printAnswer(answer)
+                if section == "3":
+                    radius = int(input("Enter the radius of the circle: "))
+                    answer = myShapes.areaOfCircle(radius)
+                    setting = printAnswer(answer)
+                if section == "4":
+                    radius = int(input("Enter the radius of the base: "))
+                    answer = myShapes.circleCircumference(radius)
+                    setting = printAnswer(answer)
+                if section == "q":
+                    print("Goodbye!")
+        if path == "2":
+            while setting:
+                section = input("Which section would you like to calculate? \n[1]distanceSpeedTime \n[2]velocityAccelerationTime \n[q]uit\n")
+                if section == "1":
+                    speed = int(input("Enter speed in meters per second: "))
+                    time = int(input("Enter the time in seconds: "))
+                    answer = myPhysics.distanceSpeedTime(speed, time)
+                    setting = printAnswer(answer)
+                if section == "2":
+                    initial_v = int(input("Enter the initial velocity in meters per second: "))
+                    acceleration = int(input("Enter the acceleration in meters per second squared: "))
+                    time = int(input("Enter the time in seconds: "))
+                    answer = myPhysics.velocityAccelerationTime(initial_v, acceleration, time)
+                    setting = printAnswer(answer)
+                if section == "q":
+                    print("Goodbye!")
+        if path == "3":
+            while setting:
+                section = input("Which section would you like to calculate? \n[1]calculateCurrent \n[2]calculateVoltage \n[3]calculateResistence \n[q]uit\n")
+                if section == "1":
+                    resistance = int(input("Enter the resistance: "))
+                    voltage = int(input("Enter the voltage: "))
+                    answer = myOhmsLaw.calculateCurrent(resistance, voltage)
+                    setting = printAnswer(answer)
+                if section == "2":
+                    current = int(input("Enter the current: "))
+                    resistance = int(input("Enter the resistence: "))
+                    answer = myOhmsLaw.calculateVoltage(current, resistance)
+                    setting = printAnswer(answer)
+                if section == "3":
+                    current = int(input("Enter the current: "))
+                    voltage = int(input("Enter the voltage: "))
+                    answer = myOhmsLaw.calculateResistence(voltage, current)
+                    setting = printAnswer(answer)
+                if section == "q":
+                    print("Goodbye!")
+                    setting = False
+                    pass
+        if path == "4":
+            while setting:
+                section = input("Which section would you like to calculate? \n[1]annualPercentageRate \n[2]compoundAmount \n[q]uit\n")
+                if section == "1":
+                    interest_c = int(input("Enter the interest charges: "))
+                    fees = int(input("Enter the fees: "))
+                    loan_a = int(input("Enter the loan amount: "))
+                    days = int(input("Enter the days in term: "))
+                    answer = myFinances.annualPercentageRate(interest_c, fees, loan_a, days)
+                    setting = printAnswer(answer)
+                    pass
+                if section == "2":
+                    principal = int(input("Enter the principal amount: "))
+                    rate = int(input("Enter the interest rate (in percentage without the percentage sign): "))
+                    number_c = int(input("Enter the number of times the interest compounds in a year: "))
+                    time = int(input("Enter the time in years: "))
+                    answer = myFinances.compoundAmount(principal, rate, number_c, time)
+                    setting = printAnswer(answer)
+                    pass
+                if section == "q":
+                    print("Goodbye!")
+                    setting = False
+        if path == "q":
+            print("Goodbye!")
+            return
+
+def printAnswer(answer):
+    print(f"Your answer was {answer}")
+    return False
 
 if __name__ == "__main__":
     main()
--- Lab-4/myShapes.py
+++ Lab-4/myShapes.py
@@ -1,6 +1,31 @@
 # Ethan Pham        9-25-25        Lab B
-from areaOfRectangle import areaOfRectangle
-from rectanglePerimeter import rectanglePerimeter
-from areaOfCircle import areaOfCircle
-from circleCircumference import circleCircumference
+# This application stores functions/calculations
+import math
 
+def areaOfRectangle(b, h):
+    # Citation - https://www.cuemath.com/measurement/area-of-rectangle/
+    # Citation - Author: Cuemath
+    # Citation - Date Published: 10-23-2021
+    # Citation - Date Accessed: 9-11-25
+    return b * h
+
+def rectanglePerimeter(l, w):
+    # Citation - https://www.smartick.com/blog/mathematics/geometry/how-to-calculate-perimeters-1/
+    # Citation - Author: Smartick
+    # Citation - Date Written - 6-3-2019
+    # Citation - Date Accessed: 9-11-25
+    return 2 * (l + w)
+
+def areaOfCircle(r):
+    # Citation - https://www.khanacademy.org/math/8th-grade-foundations-engageny/8th-m5-engage-ny-foundations/8th-m5-tb-foundations/a/area-of-a-circle
+    # Citation - Author: Khan Academy
+    # Citation - Date Published: 12-23-2019
+    # Citation - Date Accessed: 9-11-25
+    return math.pi * (r ** 2)
+
+def circleCircumference(r):
+    # Citation - https://www.khanacademy.org/a/radius-diameter-circumference
+    # Citation - Author: Khan Academy
+    # Citation - Date Published:2-12-2020 
+    # Citation - Date Accessed: 9-11-25
+    return 2 * math.pi * r
