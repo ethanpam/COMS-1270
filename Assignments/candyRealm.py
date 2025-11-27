02109f2ddf2647081e4b525fc9cd0610d5f44b6d
Ethan Pham <ethanpham652@gmail.com>
2025-11-27 07:08:40 -0600

    more update on candy realm

--- Assignments/candyRealm.py
+++ Assignments/candyRealm.py
@@ -23,9 +23,17 @@ def instruction():
     print("-When the deck runs out, it is reshuffled automatically")
     while True:
         if input("\nEnter [back] to return to main menu: ") == "back":
-            return
+            return main()
 
-def candyRealm():
+def shuffling(word, shuffle):
+    for t in range(4):
+        print(f"\rShuffling {word}{t*'.'}", end='')
+        time.sleep(.5)
+    random.shuffle(shuffle)
+    print()
+    return shuffle
+
+def playerAssign():
     players = [
         {'name': 'Bot1' ,'Bot' : True, 'tile': 1},
         {'name': 'Bot2' ,'Bot' : True, 'tile': 1},
@@ -33,32 +41,106 @@ def candyRealm():
         {'name': 'Bot4' ,'Bot' : True, 'tile': 1}
     ]
     while True:
-        for i in range(int(input("How many human player? [1-4]: "))):
+        try:
+            count = int(input("How many human player? [1-4]: "))
+            if 1 <= count <= 4:
+                break
+            else:
+                print("Enter value between 1-4")
+        except ValueError:
+            print("Invalid input")
+    for i in range(int(count)):
             player = input(f"What is the name of player {i + 1}: ")
             players[i]['name'] = player
             players[i]['Bot'] = False
-        break
-    for t in range(4):
-        print(f"\rShuffling player order{t*'.'}", end='')
-        time.sleep(1)
-    random.shuffle(players)
-    print(f"Player Order:\n")
-    return print(players)
+    player = shuffling("player order", players)
+    print(f"\nPlayer Order:\n1.{players[0]['name']}\n2.{players[1]['name']}\n3.{players[2]['name']}\n4.{players[3]['name']}")
+    return players
 
+def card():
+    deck = []
+    while True:
+        try:
+            copies = int(input("How many copies of each cards [1-5]?: "))
+            if 1 <= copies <= 5:
+                break
+            else:
+                print("Enter a value between [1-5]")
+        except ValueError:
+            print("Invalid Input")
+    print("Adding Cards")
+    for _ in range(copies):
+        deck.append("R")
+        deck.append("P")
+        deck.append("Y")
+        deck.append("B")
+        deck.append("O")
+        deck.append("G")
+    deck = shuffling("deck", deck)
+    return deck
 
+def playingBoard(players, deck):
+    board = []
+    for _ in range(3):
+        board.append("R")
+        board.append("P")
+        board.append("Y")
+        board.append("B")
+        board.append("O")
+        board.append("G")
+    random.shuffle(board)
+    board.append("GOAL!")
+    print(f"{players[0]["name"]:>11}")
+    print(f"{players[1]["name"]:>11}")
+    print(f"{players[2]["name"]:>11}")
+    print(f"{players[3]["name"]:>11}")
+    print(f"{"START":>7}",end='')
+    for title in board[:18]:
+        print(f"{title:>4}", end='')
+    print(f"{"GOAL!":>7}",end='')
+    print()
+    print(f"{"CARDS":>7}", end='')
+    for card in deck[:18]:
+        print(f"{card:>4}", end='')
+    print()
+
+def turn(players, deck):
+    playerTurn = 0
+    while True:
+        playingBoard(players, deck)
+        choice = input(f"{players[playerTurn]["name"]}: Would you like to [d]raw a {deck[0]} card, [s]huffle the deck, or [q]uit: ")
+        if choice.lower() == "d":
+            draw(players, deck)
+        elif choice.lower() == "s":
+            shuffling("deck", deck)
+        elif choice.lower() == "q":
+            return main()
+        playerTurn += 1
+        if playerTurn >= len(players):
+            playerTurn = 0
+
+def draw(players, deck):
+    pass
+
+def candyRealm():
+    players = playerAssign()
+    deck = card()
+    turn(players, deck)
 
 def main():
-    printTitleMaterial()
     while True:
         print("-----------------------")
         choice = input("MAIN MENU: [p]lay game, [i]nstructions, or [q]uit: ")
         if choice == "p":
             candyRealm()
+            return
         elif choice == "i":
             instruction()
+            return
         elif choice == "q":
             print("\nHave a good day, goodbye!\n")
             return
 
 if __name__ == "__main__":
+    printTitleMaterial()
     main()
